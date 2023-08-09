dde793265fc20fe388d26fc201980ff4e126093e
Bongsug Chae <bchae@users.campus>
2023-08-09 15:24:25 -0500

    initial commit

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ st.image("https://a.espncdn.com/i/teamlogos/mlb/500/kc.png")
 st.markdown("""
 This app performs simple webscraping of NBA player stats data!
 * **Python libraries:** base64, pandas, streamlit
-* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/) & specifically from [here](https://www.basketball-reference.com/leagues/).
 """)
 
 st.sidebar.header('User Input Features')
 selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
 
 # Web scraping of NBA player stats
-@st.cache
+@st.cache_data
 def load_data(year):
     url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
     html = pd.read_html(url, header = 0)
@@ -70,7 +70,7 @@ if st.button('Intercorrelation Heatmap'):
     with sns.axes_style("white"):
         f, ax = plt.subplots(figsize=(7, 5))
         ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-    st.pyplot()
+    st.pyplot(f)
 
 
 st.markdown("## Party time!")
